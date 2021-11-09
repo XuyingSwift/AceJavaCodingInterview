807b2471f92b6f2f80e85cbfe364ca6233f69367
XuyingSwift <xuyingswift@gmail.com>
2021-11-09 11:51:09 -0700

    2D arrays

--- src/main/DataStructure/Arrays/ChangeOneDArrayTo2DArray.java
+++ src/main/DataStructure/Arrays/ChangeOneDArrayTo2DArray.java
@@ -1,2 +1,31 @@
-package main.DataStructure.Arrays;public class ChangeOneDArrayTo2DArray {
+package main.DataStructure.Arrays;
+
+public class ChangeOneDArrayTo2DArray {
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,6};
+        int row = 2;
+        int col = 3;
+
+        int[][] b = changeOneToTwo(a, row, col);
+        for (int i =0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                System.out.print(b[i][j]);
+            }
+        }
+    }
+
+    public static int[][] changeOneToTwo(int[] a, int row, int col) {
+        if (row*col < a.length || row*col > a.length) {
+            return new int[0][0];
+        }
+        int[][] arr = new int[row][col];
+        int index =0;
+        for(int i=0; i<row; i++) {
+            for (int j =0; j<col; j++) {
+                arr[i][j] = a[index++];
+            }
+        }
+        return arr;
+    }
 }
--- src/main/DataStructure/Arrays/FindTargetIn2D.java
+++ src/main/DataStructure/Arrays/FindTargetIn2D.java
@@ -1,2 +1,46 @@
-package main.DataStructure.Arrays;public class FindTargetIn2D {
+package main.DataStructure.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindTargetIn2D {
+
+    public static void main(String[] args) {
+        int[][] a = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}};
+        int target = 4;
+
+        System.out.println(findTarget(a, target));
+        System.out.println(searchMatrix(a, target));
+    }
+
+    public static boolean findTarget(int[][] a, int target) {
+
+        for (int i =0; i<a.length; i++) {
+            for (int j =0; j<a[i].length; j++) {
+                if (target == a[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length == 0 || matrix == null) return false;
+        int row = 0, col = matrix[0].length - 1;
+
+        while(row < matrix.length && col >= 0){
+            if(matrix[row][col] == target){
+                return true;
+            }else if(matrix[row][col] < target){
+                row++;
+            }else{
+                col--;
+            }
+        }
+        return false;
+    }
 }
